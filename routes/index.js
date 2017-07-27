d09ffd9c8a5b623379028dd08c9a482b977f0c18
jdrichardstech <jdrichardstech@gmail.com>
2017-07-27 18:18:10 -0400

    create dummy dropdown

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const request = require('request')
 const router = express.Router();
+const attachments = require('../lib/slack/attachments')
+const cities = require('../lib/cities')
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
@@ -9,6 +11,42 @@ router.get('/', (req, res, next) => {
 
 router.post('/helloworld',(req, res, next) => {
 	res.json({"text":"Hello World"})
+	res.end()
+})
+
+router.post('/city',(req, res) => {
+	console.log("OPENING PAY: " + JSON.stringify(req.body))
+	res.json({
+						"Content-type": "application/json",
+						"response_type": 'ephemeral',
+						"text": `Hi ${req.body.user_name}!`,
+						"attachments": [
+							{
+               "text":"What city are you from:",
+               "mrkdwn": true,
+               "fallback": "You are unable to choose a city",
+               "callback_id": "city",
+               "attachment_type": "default",
+               "actions":[
+                 {
+                  "name": 'city',
+                  "text": 'City',
+                  "type": 'select',
+                  "value": 'city',
+                  "style": 'primary',
+                  "options": attachments.loopCities(cities)
+                 }
+               ]
+						  }
+						]
+	})
+	res.end()
+})
+
+router.post('/showcity', (req, res) => {
+	let payload = JSON.parse(req.body.payload)
+	console.log("PARSE: " + JSON.stringify(JSON.parse(req.body.payload)))
+	res.json({"text":`${payload.user.name} chose *${payload.actions[0].selected_options[0].value}*`})
 	res.end()
 })
 
