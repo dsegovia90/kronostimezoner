53b3c2bb353a05e9d54019b853b599dbd7de02f5
jdrichardstech <jdrichardstech@gmail.com>
2017-07-28 16:07:17 -0400

    edited Users model

--- models/Users.js
+++ models/Users.js
@@ -5,13 +5,10 @@ const UserSchema = new Schema({
 	token:{type: String},
     teamID: {type: String},
     teamDomain:{type:String},
-    channelID: {type: String},
-    channelName: {type: String},
     userID: {type: String},
-	userName:{type:String, default:''},
-	timeStamp:{type:Date, default:Date.now}
+	userName:{type:String}
 })
 
-const Run = mongoose.model('User', UserSchema)
+module.exports = mongoose.model('User', UserSchema)
 
-module.exports = User
+
