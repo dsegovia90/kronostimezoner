b291f3ae9e71c8e2d8d6fcad55b0615ac8a27eb8
jdrichardstech <jdrichardstech@gmail.com>
2017-07-29 15:32:05 -0400

    create time sending ability local to each user

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,12 @@ const router = express.Router();
 const attachments = require('../lib/slack/attachments')
 const User = require('../models/Users')
 const cities = require('../lib/cities')
+const slack = require('slack')
 
 /* GET home page. */
 router.get('/', (req, res) => {
   res.render('index', { title: 'Slack-Timezoner' });
+  
 });
 
 router.post('/helloworld', (req, res) => {
@@ -65,6 +67,38 @@ router.post('/showcity', (req, res) => {
   let payload = JSON.parse(req.body.payload)
   console.log("PARSE: " + JSON.stringify(JSON.parse(req.body.payload)))
   res.json({ "text": `${payload.user.name} chose *${payload.actions[0].selected_options[0].value}*` })
+})
+
+router.post('/sendtime',(req,res) => {
+  let channel = req.body.channel_id
+  let token = process.env.VERIFICATION_TOKEN
+  let unixDate = Math.round((new Date()).getTime() / 1000)
+  let text = `<!date^${unixDate}^The time and date is {date} at {time}.|Can we meet soon?>`
+  slack.users.list({token}, (err, data) => {
+    // console.log("DATA: " + JSON.stringify(data))
+    let usersArr = [] 
+    let users = ''
+    
+    //create array of user id's
+    data.members.forEach((user)=>{
+      usersArr.push(user.id)
+    })
+
+    //remove the bot id as it can't join
+    usersArr.pop(usersArr.length-1)
+    users = usersArr.join(",")
+    console.log("USERS: " + users)
+  
+    //open multiperson instant message using slackjs module
+    slack.mpim.open({token, users}, (err, data) => { 
+      //send request for meeting using slackjs module
+      slack.chat.postMessage({token, channel, text}, (err, data) => { 
+      if(err){
+        console.log("POST ERROR: " + JSON.stringify(err))
+      }
+    })
+   })
+  })
 })
 
 module.exports = router;
