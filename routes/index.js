4bf1dd5ba1644fcf22b00aa9f1c1d5ee4722bbb7
Daniel Segovia <segoarce90@gmail.com>
2017-08-07 13:26:48 -0500

    fix indent issues

--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,15 @@ const router = express.Router();
 const slack = require('slack')
 
 router.get('/', (req, res) => {
-	res.render('index', {title: 'Slack Timezoner', installButtonLink: process.env.INSTALL_BUTTON_LINK}) //this link is unique to each app
+  res.render('index', { title: 'Slack Timezoner', installButtonLink: process.env.INSTALL_BUTTON_LINK }) //this link is unique to each app
 })
 
 //slack button route
-router.get('/install', (req, res)=>{
+router.get('/install', (req, res) => {
   let client_id = process.env.SLACK_CLIENT_ID
   let client_secret = process.env.SLACK_CLIENT_SECRET
   let code = req.query.code
-  slack.oauth.access({client_id, client_secret, code}, (err, data) => {
+  slack.oauth.access({ client_id, client_secret, code }, (err, data) => {
     /* we need to handle the error here and store the data if successfull. */
     res.redirect('/')
   })
@@ -21,8 +21,8 @@ router.post('/catchmessages', (req, res) => {
   res.send(req.body.challenge)
   let receivedText = req.body.event.text
   let timeRegex = /\d{1,2}:\d{2}(pm|am)?/i
-  
-  if(timeRegex.test(receivedText) && !req.body.event.subtype){
+
+  if (timeRegex.test(receivedText) && !req.body.event.subtype) {
     // Everything needed for the slack api interaction except text:
     let token = process.env.VERIFICATION_TOKEN //extracted from db in the real world
     let channel = req.body.event.channel
@@ -31,30 +31,30 @@ router.post('/catchmessages', (req, res) => {
     // Capture the time the user sent via slack, and split it by the ':'
     let capturedTime = receivedText.match(timeRegex)[0].split(':')
 
-     // SEPARATE HOUR AND MINUTES FOR LATER USE
-     // Determine whether  input time is am or pm
-    let capturedAmPm=capturedTime[1].substring(2)
-     //Assign hour according to whether it is am or pm
-    let capturedHour = capturedAmPm =='am' ? parseInt(capturedTime[0]) : parseInt(capturedTime[0])+12
+    // SEPARATE HOUR AND MINUTES FOR LATER USE
+    // Determine whether  input time is am or pm
+    let capturedAmPm = capturedTime[1].substring(2)
+    //Assign hour according to whether it is am or pm
+    let capturedHour = capturedAmPm == 'am' ? parseInt(capturedTime[0]) : parseInt(capturedTime[0]) + 12
     // let capturedHour = parseInt(capturedTime[0]) //this only outputs am time
-    let capturedMinutes = capturedTime[1] ? parseInt(capturedTime[1].substring(0,2)) : 0
-    
+    let capturedMinutes = capturedTime[1] ? parseInt(capturedTime[1].substring(0, 2)) : 0
+
     //get the current date in UTC to know the year, month and day in UTC
-    let utcDate = new Date(); 
+    let utcDate = new Date();
 
     //use utcDate to create a new date object with the capturedHour and capturedMinutes
-    let utcProjectedTime = new Date( 
+    let utcProjectedTime = new Date(
       Date.UTC(
-        utcDate.getUTCFullYear(), 
-        utcDate.getUTCMonth(), 
-        utcDate.getUTCDate(), 
-        capturedHour, 
+        utcDate.getUTCFullYear(),
+        utcDate.getUTCMonth(),
+        utcDate.getUTCDate(),
+        capturedHour,
         capturedMinutes
       )).getTime() / 1000
 
     // This promise fetches the user info (the user is the one who sent the message)
     let userInfoPromise = new Promise((resolve, reject) => {
-      slack.users.info({token, user}, (err, data) => {
+      slack.users.info({ token, user }, (err, data) => {
         resolve(data)
         reject(err)
       })
@@ -70,18 +70,18 @@ router.post('/catchmessages', (req, res) => {
 
       // This creates the message, needs formatting. 
       let text = `<!date^${unixDate}^The translation of that time is {time}.|Can we meet soon?>`
-      
+
       // Return a promise to catch any errors. 
       return postMessagePromise = new Promise((resolve, reject) => {
-        slack.chat.postMessage({token, channel, text}, (err, data) => {
+        slack.chat.postMessage({ token, channel, text }, (err, data) => {
           resolve(data)
           reject(err)
         })
       })
     })
-    .catch((err) => {
-      console.error(err)
-    })
+      .catch((err) => {
+        console.error(err)
+      })
 
   }
 })
