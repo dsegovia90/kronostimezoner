a7f43e4c40e16a6fb0cc0afd1af39f61001154fe
Daniel Segovia <segoarce90@gmail.com>
2017-08-08 12:39:19 -0500

    added the slack route

--- app.js
+++ app.js
@@ -35,6 +35,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
+app.use('/slack', slack);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
