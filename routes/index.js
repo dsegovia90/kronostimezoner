6755979bdd45a2d359e04d7f05cc4630bc81f6ce
jdrichardstech <jdrichardstech@gmail.com>
2017-08-09 19:20:14 -0400

    removed typo

--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,15 @@ const router = express.Router();
 
 
 router.get('/', (req, res) => {
-  res.render('index', { isActive:true, title: 'Timezoner', installButtonLink: process.env.INSTALL_BUTTON_LINK }) //this link is unique to each app
+  res.render('index', { title: 'Timezoner', installButtonLink: process.env.INSTALL_BUTTON_LINK }) //this link is unique to each app
 })
 
 router.get('/privacy', (req,res) => {
-  res.render('privacy', {isActive:true, title: 'Timezoner', installButtonLink: process.env.INSTALL_BUTTON_LINK })
+  res.render('privacy', {title: 'Timezoner', installButtonLink: process.env.INSTALL_BUTTON_LINK })
 })
 
 router.get('/support', (req,res) => {
-  res.render('support', {isActive:true, title: 'Timezoner', installButtonLink: process.env.INSTALL_BUTTON_LINK })
+  res.render('support', {title: 'Timezoner', installButtonLink: process.env.INSTALL_BUTTON_LINK })
 })
 
 module.exports = router;
