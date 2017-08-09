66551f97d1701cf53600599b794add381b5bd7f6
jdrichardstech <jdrichardstech@gmail.com>
2017-08-09 13:25:13 -0400

    initial landing page commit

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
+
 router.get('/', (req, res) => {
   res.render('index', { title: 'Slack Timezoner', installButtonLink: process.env.INSTALL_BUTTON_LINK }) //this link is unique to each app
+})
+
+router.get('/privacy', (req,res) => {
+  res.render('privacy', null)
+})
+
+router.get('/support', (req,res) => {
+  res.render('support', null)
 })
 
 module.exports = router;
