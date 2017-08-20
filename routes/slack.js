79ee95fc08f6404c00626ccaae659058f7359f8f
jdrichardstech <jdrichardstech@gmail.com>
2017-08-20 16:04:15 -0400

    allow for space between am and pm

--- routes/slack.js
+++ routes/slack.js
@@ -41,10 +41,14 @@ router.get('/install', (req, res) => {
 router.post('/catchmessages', (req, res) => {
   res.send(req.body.challenge)
   let receivedText = req.body.event.text
+  //split receivedText so that you can find the am pm after the space as an index
+  let receivedTextArr = receivedText.toLowerCase().split(' ')
+  console.log("TEXT ARRAY: " + receivedTextArr)
   let timeRegex = /\d{1,2}:\d{2}(pm|am)?/i
-
   if (timeRegex.test(receivedText) && !req.body.event.subtype) {
     console.log('received a time!')
+
+    
     // Everything needed for the slack api interaction except text:
     let channel = req.body.event.channel
     let user = req.body.event.user
@@ -54,8 +58,20 @@ router.post('/catchmessages', (req, res) => {
     let capturedTime = receivedText.match(timeRegex)[0].split(':')
     // SEPARATE HOUR AND MINUTES FOR LATER USE
     // Set user input time to am if no stipulation otherwise assign am or pm to variable
-    let capturedAmPm = capturedTime[1].substring(2).length == 0 ? 'am' : capturedTime[1].substring(2)
-
+    //use to check for ap pm after capturedTime and a space
+    let checkTime = capturedTime.join(':')
+    let wordAfter = receivedTextArr[receivedTextArr.indexOf(checkTime)+1]
+    console.log("AFTER:" + wordAfter)
+    console.log(checkTime)
+    // if(capturedTime[1].substring(2).length == 0 || wordAfter != 'pm'){
+    //   let capturedAmPm = 'am'
+    // }else if(wordAfter == 'pm'){
+    //   let capturedAmPm = 'pm'
+    // }else{
+    //   capturedAmPm = capturedTime[1].substring(2)
+    // }
+    let capturedAmPm = wordAfter == 'pm' ? 'pm' : capturedTime[1].substring(2).length == 0 ? 'am': capturedTime[1].substring(2)
+    console.log("SUB: " + capturedTime[1].substring(2))
     //handle 12:00/12:00am/12:00pm user input
     if (capturedAmPm == 'am') {
       if ((capturedTime[1].substring(2).length == 2 && capturedTime[0] == '12') || (capturedTime[1].substring(2).length == 0 && capturedTime[0] == '12')) {
