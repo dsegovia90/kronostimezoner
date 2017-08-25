80eab78ab69e07b32686806ed92d7761d0520b75
Daniel Segovia <segoarce90@gmail.com>
2017-08-25 14:23:34 -0500

    linted and working

--- app.js
+++ app.js
@@ -1,34 +1,34 @@
 const express = require('express');
 const path = require('path');
-const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const index = require('./routes/index');
 const slack = require('./routes/slack');
-const mongoose = require('mongoose')
-const routes = require('./routes/index')
-require('dotenv').config()
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const app = express();
 
-mongoose.Promise = global.Promise
-const databaseUri= process.env.MONGO_URI
+mongoose.Promise = global.Promise;
+const databaseUri = process.env.MONGO_URI;
 // mongoose.connect(databaseUri, { useMongoClient: true })
-mongoose.connect(databaseUri,{ useMongoClient: true }, (err, res) => {
-  if(err){
-    console.log('DB CONNECTION FAIL: ' + err)
-  }else{
-    console.log('DB CONNECTION SUCCESS: ' )
-  }
-})
+mongoose.connect(
+  databaseUri,
+  { useMongoClient: true },
+    (err, res) => { // eslint-disable-line no-unused-vars
+      if (err) {
+        console.log(err);
+      } else {
+        console.log('Database Connection Successfull.');
+      }
+    });
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -38,22 +38,20 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/slack', slack);
 
-
-
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  let err = new Error('Not Found');
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  console.error(err)
+  console.error(err);
 
   // render the error page
   res.status(err.status || 500);
