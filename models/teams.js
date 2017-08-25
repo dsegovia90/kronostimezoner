26c19fff39d84b11d88b1d92dd17d4a0057614df
Daniel Segovia <segoarce90@gmail.com>
2017-08-25 14:27:29 -0500

    linted and working

--- models/teams.js
+++ models/teams.js
@@ -1,13 +1,13 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const TeamsSchema = new mongoose.Schema({
-  accessToken:{type:String},
-  userId:{type: String},
-  scope:{type:String},
-  teamName:{type:String},
-  teamId: {type:String},
-  lastUpdate: {type:Date, default:new Date()},
-  count: {type: Number, default: 0}
-})
+  accessToken: { type: String },
+  userId: { type: String },
+  scope: { type: String },
+  teamName: { type: String },
+  teamId: { type: String },
+  lastUpdate: { type: Date, default: new Date() },
+  count: { type: Number, default: 0 },
+});
 
-module.exports = mongoose.model('Teams', TeamsSchema)
+module.exports = mongoose.model('Teams', TeamsSchema);
