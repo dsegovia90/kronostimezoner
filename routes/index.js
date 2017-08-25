d4687c8705d1bf5ad54700197453e3a9b8d65d71
Daniel Segovia <segoarce90@gmail.com>
2017-08-25 14:16:33 -0500

    linted and testing

--- routes/index.js
+++ routes/index.js
@@ -1,43 +1,45 @@
 const express = require('express');
+
 const router = express.Router();
 const Team = require('../models/teams.js');
 
 router.use((req, res, next) => {
-  res.locals.title = 'Kronos Timezoner'
-  res.locals.installButtonLink = process.env.INSTALL_BUTTON_LINK
+  res.locals.title = 'Kronos Timezoner';
+  res.locals.installButtonLink = process.env.INSTALL_BUTTON_LINK;
 
-  let getCountPromise = Team.find({})
-  let num = 0
+  const getCountPromise = Team.find({});
+  let num = 0;
 
   getCountPromise.then((teams) => {
     teams.forEach((team) => {
-      num += team.count
-    })
-    res.locals.num = num
-    next()
+      num += team.count;
+    });
+    res.locals.num = num;
+    next();
   }).catch((err) => {
-    res.locals.num = null
-    next()
-  })
-})
+    console.log(err);
+    res.locals.num = null;
+    next();
+  });
+});
 
 router.get('/', (req, res) => {
-  let message = req.query.install === 'unsuccessful' ?
+  const message = req.query.install === 'unsuccessful' ?
     'App could not be installed. Please contact support.' :
-    null
-  res.render('index', { message })
-})
+    null;
+  res.render('index', { message });
+});
 
 router.get('/privacy', (req, res) => {
-  res.render('privacy')
-})
+  res.render('privacy');
+});
 
 router.get('/support', (req, res) => {
-  res.render('support')
-})
+  res.render('support');
+});
 
 router.get('/thanks', (req, res) => {
-  res.render('thanks')
-})
+  res.render('thanks');
+});
 
 module.exports = router;
