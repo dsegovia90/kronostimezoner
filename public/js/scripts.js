61dac46d44dd4fbdb38386c865ebb9e344f674f7
Daniel Segovia <segoarce90@gmail.com>
2017-10-09 16:26:54 -0700

    changing the count in realtime

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,1 +1,5 @@
-io.connect('http://localhost:8000/');
+const socket = io.connect('http://localhost:8000/');
+
+socket.on('newCount', (data) => {
+  document.getElementById('translation-count').innerHTML = data.newCount;
+});
